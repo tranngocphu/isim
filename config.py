c04bde566c565e74009d95db2df7f13a43076367
Phu <tr.ngocphu@gmail.com>
2020-06-29 02:17:44 +0800

    remove mysql engine from the project

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-
 # Global configurations
 APP_NAME = 'Intelligent Air Traffic Simulator'
+
 
 # Web server configuration
 SERVER_PORT   = 8888
@@ -12,13 +12,5 @@ STATIC_ROOT   = os.path.join(TEMPLATE_ROOT, "static")
 
 
 # SQL Database configuration
-SQL_MODE = 'sqlite' # 'sqlite' or 'mysql
-HOST     = '127.0.0.1'
-DATABASE = 'isim'
-USER     = 'root'
-PASSWORD = 'root'
-SQL_PORT = '3306'
 SQLITE_PATH = os.path.join(SERVER_ROOT, "bin", "sqlite", "isim.db")
-DB_URL   = "mysql+mysqldb://{}:{}@{}:{}/{}".format(USER, PASSWORD, HOST, SQL_PORT, DATABASE) if SQL_MODE == 'mysql' else "sqlite:///{}".format(SQLITE_PATH)
-
-
+DB_URL = "sqlite:///{}".format(SQLITE_PATH)
