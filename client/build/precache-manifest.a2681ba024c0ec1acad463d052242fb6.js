dea7d71976c938e849e61f757bee8ab4a77a24f7
Phu <tr.ngocphu@gmail.com>
2020-07-02 18:38:57 +0800

    add frontend build folder

---
+++ client/build/precache-manifest.a2681ba024c0ec1acad463d052242fb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f9ca7502dfe4a74e8a44629dce02c10",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e9ad7967df415a98a86",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fadc2ca15f1703145b57",
+    "url": "/static/js/2.22b19acc.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.22b19acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9ad7967df415a98a86",
+    "url": "/static/js/main.7a69c698.chunk.js"
+  },
+  {
+    "revision": "6dc4c297a8a747c805d8",
+    "url": "/static/js/runtime-main.ee1cbfe1.js"
+  }
+]);
